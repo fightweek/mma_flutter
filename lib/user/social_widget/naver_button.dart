4d64728a66271b05181069fe4a030b7e82119f8d
JINHYUNTAEK <jht2512@naver.com>
2025-05-02 15:27:49 -0700

    naver_login_succeed

---
+++ lib/user/social_widget/naver_button.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:mma_flutter/user/model/naver_login_request.dart';
+import 'package:mma_flutter/user/model/user_model.dart';
+import 'package:mma_flutter/user/provider/user_provider.dart';
+import 'package:naver_login_sdk/naver_login_sdk.dart';
+import 'package:flutter_logcat/flutter_logcat.dart';
+
+class NaverButton extends ConsumerWidget {
+  const NaverButton({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final userNotifier = ref.read(userProvider.notifier);
+    final user = ref.watch(userProvider);
+    final isLoading = user is UserModelLoading;
+
+    return NaverLoginButton(
+      onPressed:
+          isLoading
+              ? () {
+                print('button blocked for some reason!');
+              }
+              : () {
+                userNotifier.setStateLoading();
+                NaverLoginSDK.authenticate(
+                  callback: OAuthLoginCallback(
+                    onSuccess: () async {
+                      Log.d('Naver login succeed');
+                      final accessToken = await NaverLoginSDK.getAccessToken();
+                      NaverLoginSDK.profile(
+                        callback: ProfileCallback(
+                          onSuccess: (resultCode, message, response) {
+                            Log.i('Get naver profile...');
+                            final profile = NaverLoginProfile.fromJson(
+                              response: response,
+                            );
+                            userNotifier.naverLogin(
+                              request: NaverLoginRequest(
+                                naverAccessToken: accessToken,
+                                email: profile.email!,
+                                socialId: profile.id!,
+                                nickname: profile.nickName!,
+                              ),
+                            );
+                          },
+                        ),
+                      );
+                    },
+                    onError: (errorCode, message) {},
+                    onFailure: (httpStatus, message) {},
+                  ),
+                );
+              },
+      fit: BoxFit.cover,
+      height: 50,
+      width: MediaQuery.of(context).size.width,
+      style: NaverLoginButtonStyle(
+        mode: NaverButtonMode.white,
+        type: NaverButtonType.rectangleBar,
+        language: NaverButtonLanguage.korean,
+      ),
+    );
+  }
+}
