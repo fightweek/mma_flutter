6c500fb14958b49c747160ffc52c902d996417fe
JINHYUNTAEK <jht2512@naver.com>
2025-05-04 01:45:13 -0700

    feature/general_login_error_handling

--- lib/common/component/custom_text_form_field.dart
+++ lib/common/component/custom_text_form_field.dart
@@ -3,21 +3,27 @@ import 'package:mma_flutter/common/const/colors.dart';
 
 class CustomTextFormField extends StatelessWidget {
   final String? hintText;
-  final String? errorText;
+  final String? theme;
   final bool obscureText;
   final bool autofocus;
+  final FormFieldValidator? validator;
   final ValueChanged<String>? onChanged;
 
-  const CustomTextFormField({required this.onChanged, this.obscureText = false, this.autofocus=false, this.hintText,this.errorText, super.key});
+  const CustomTextFormField({
+    required this.onChanged,
+    this.obscureText = false,
+    this.autofocus = false,
+    this.hintText,
+    this.theme,
+    this.validator,
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
-    final baseBorder = OutlineInputBorder(
-        borderSide: BorderSide(
-          width: 1.0,
-        )
-    );
+    final baseBorder = OutlineInputBorder(borderSide: BorderSide(width: 1.0));
     return TextFormField(
+      validator: validator,
       cursorColor: PRIMARY_COLOR,
       // 비밀번호 입력할 때
       obscureText: obscureText,
@@ -26,20 +32,14 @@ class CustomTextFormField extends StatelessWidget {
       decoration: InputDecoration(
         contentPadding: EdgeInsets.all(20),
         hintText: hintText,
-        errorText: errorText,
-        hintStyle: TextStyle(
-          color: Colors.grey,
-          fontSize: 14.0,
-        ),
+        hintStyle: TextStyle(color: Colors.grey, fontSize: 14.0),
         fillColor: Colors.white,
         filled: true,
         border: baseBorder,
         // 텍스트 필드 선택 시 border 적용
         enabledBorder: baseBorder,
         focusedBorder: baseBorder.copyWith(
-            borderSide: baseBorder.borderSide.copyWith(
-                color: PRIMARY_COLOR
-            )
+          borderSide: baseBorder.borderSide.copyWith(color: PRIMARY_COLOR),
         ),
       ),
     );
