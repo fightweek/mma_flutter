b2703425b33aae7d33742f1628baa6fa0944ac49
JINHYUNTAEK <jht2512@naver.com>
2025-05-03 22:20:46 -0700

    feature/google_login

---
+++ lib/user/service/kakao_login_service.dart
@@ -0,0 +1,22 @@
+import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
+import 'package:mma_flutter/user/enumtype/login_platform.dart';
+import 'package:mma_flutter/user/model/naver_login_request.dart';
+
+class KakaoLoginService {
+  static Future<SocialLoginRequest> login() async {
+    print('domain = ${ LoginPlatform.kakao.name.toUpperCase()}');
+    bool isInstalled = await isKakaoTalkInstalled();
+    final OAuthToken accessToken =
+        isInstalled
+            ? await UserApi.instance.loginWithKakaoTalk()
+            : await UserApi.instance.loginWithKakaoAccount();
+    User user = await UserApi.instance.me();
+    return SocialLoginRequest(
+      domain: LoginPlatform.kakao.name.toUpperCase(),
+      accessToken: accessToken.accessToken,
+      email: user.kakaoAccount!.email!,
+      socialId: user.id.toString(),
+      nickname: user.kakaoAccount!.profile!.nickname!,
+    );
+  }
+}
